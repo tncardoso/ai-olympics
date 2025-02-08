5c12262000ff4e756132532333572bfabd8525bf
Thiago Cardoso <thiagoncc@gmail.com>
2025-02-08 18:44:06 -0300

    Index + base dir update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Github } from 'lucide-svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen text-center p-4">
+    <h1 class="text-5xl font-bold mb-4">AI Olympics</h1>
+    <p class="text-xl mb-6 mt-6">AI Olympics is a collection of unusual benchmarks designed to evaluate AI models.</p>
+    
+    <a href="https://github.com/tncardoso/ai-olympics" target="_blank" class="mt-4 text-gray-700 hover:text-gray-900">
+       <Github />
+    </a>
+</div>
+
--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+    paths: {
+      base: '/ai-olympics',
+    },
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
